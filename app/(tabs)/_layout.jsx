131863d3ca67815fae12bf7a4468c1cedc3927cc
Jaco-Miras <miras.jaco@gmail.com>
2024-04-14 18:04:32 +0800

    innitial commit

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,13 +1,93 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { Tabs, Redirect } from "expo-router";
+
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+    </View>
+  );
+};
 
 export default function TabsLayout() {
   return (
     <>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: "white",
+          },
+        }}
+      >
         <Tabs.Screen
           name="home"
-          options={{ title: "Home", headerShown: false }}
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
         />
       </Tabs>
     </>
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import {
+  Button,
+  ScrollView,
+  Text,
+  View,
+  StyleSheet,
+  TouchableHighlight,
+} from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">wow</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Profile
-      </Link>
-    </View>
+    <SafeAreaView className="h-full bg-slate-100 flex items-center justify-center flex-1">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="flex-row flex-wrap justify-center gap-x-6 mt-10">
+          <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="#DDDDDD"
+            onPress={() => alert("Pressed!")}
+            className="bg-white p-4 border border-gray-200 w-[100px] h-20 flex items-center justify-center rounded"
+          >
+            <Text className="text-center">1</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="#DDDDDD"
+            onPress={() => alert("Pressed!")}
+            className="bg-white p-4 border border-gray-200 w-[100px] h-20 flex items-center justify-center rounded"
+          >
+            <Text className="text-center">2</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="#DDDDDD"
+            onPress={() => alert("Pressed!")}
+            className="bg-white p-4 border border-gray-200 w-[100px] h-20 flex items-center justify-center rounded"
+          >
+            <Text className="text-center">3</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View className="">
+          <Text>sss</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
